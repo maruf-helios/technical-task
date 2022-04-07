2ed61b8768bf6e60b59150f3ce5730c575d30180
marufhelios <maruf.hosen@heliosinsider.com>
2022-04-07 08:31:36 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import InputUser from "./component/InputUser";
+
+function App() {
+  return (
+    <div className="App">
+      <InputUser />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/InputUser.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const InputUser = () => {
+    return (
+        <div>
+            User
+        </div>
+    );
+};
+
+export default InputUser;
